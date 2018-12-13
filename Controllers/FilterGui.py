7e6670d0c94258f03e2526fd81ae7f049f114569
kewan <khkewan@gmail.com>
2018-12-13 15:15:16 +0200

    last virsion

--- Controllers/FilterGui.py
+++ Controllers/FilterGui.py
@@ -1,14 +1,23 @@
 # -*- coding: utf-8 -*-
-
 # Form implementation generated from reading ui file 'PyQt5.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
+import sys
+from threading import Thread
 
+import settings
 from PyQt5 import QtCore, QtGui, QtWidgets
+from matplotlib.pyplot import imshow
+from matplotlib import pyplot as plt
+
+import resources
+from Models.Filters import *
+
 
 class Ui_MainWindow(object):
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(631, 644)
@@ -65,9 +74,9 @@ class Ui_MainWindow(object):
         self.lineEdit_4 = QtWidgets.QLineEdit(self.centralwidget)
         self.lineEdit_4.setGeometry(QtCore.QRect(290, 80, 21, 20))
         self.lineEdit_4.setObjectName("lineEdit_4")
-        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(40, 150, 571, 421))
-        self.graphicsView.setObjectName("graphicsView")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(10, 150, 661, 391))
+        self.textBrowser.setObjectName("textBrowser")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(490, 580, 75, 23))
         self.pushButton.setObjectName("pushButton")
@@ -97,23 +106,72 @@ class Ui_MainWindow(object):
         self.label_5.setText(_translate("MainWindow", "X2"))
         self.label_6.setText(_translate("MainWindow", "Y2"))
         self.pushButton.setText(_translate("MainWindow", "OK"))
+        self.textBrowser.setHtml(_translate("MainWindow",
+                                            "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                                            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                                            "p, li { white-space: pre-wrap; }\n"
+                                            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+                                            "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><img src=\":/newPrefix/paths0.png\" /></p></body></html>"))
 
     def get_filters(self):
         fil = {"f1": self.checkBox.checkState(), "f2": self.checkBox_2.checkState(),
                "f3": self.checkBox_3.checkState(), "f4": self.checkBox_4.checkState()}
-        for filter_name, status in fil:
-            if status != 0:
-                if filter_name == "f1" :
-                    print(self.timeEdit.text())
+        dr = {}
+        s = {}
+        for filter_name, status in fil.items():
+            if status == 2:
+                if filter_name == "f1":
+                    s1 = str(self.timeEdit.text()) + ":00"
+                    s["start_time"] = s1
+                    s1 = str(self.timeEdit_2.text()) + ":00"
+                    s["end_time"] = s1
+                    dr["time_filter"] = s
+                elif filter_name == "f2":
+                    s["date"] = str(self.dateEdit.text())
+                    s1 = str(self.timeEdit.text()) + ":00"
+                    s["start_time"] = s1
+                    s1 = str(self.timeEdit_2.text()) + ":00"
+                    s["end_time"] = s1
+                    dr["date_filter"] = s
+                elif filter_name == "f3":
+                    s["x1"] = int(self.lineEdit.text())
+                    s["y2"] = int(self.lineEdit.text())
+                    s["x2"] = int(self.lineEdit.text())
+                    s["y2"] = int(self.lineEdit.text())
+                    dr["location_filter"] = s
+            s = {}
+            f = Thread(target=arguments_receiver_and_filter,args=(dr,))
+            f.start()
+
+            print(f.is_alive())
+            while f.is_alive():
+
+                self.centralwidget.update()
+            print(decorated_df)
+            s = Thread(target=plots,args=(decorated_df,))
+            s.start()
+            while s.is_alive():
+                self.centralwidget.update()
+
+
+def show(objs, df_by_obj):
+    imshow(settings.bg, alpha=0.5)
+    for t, n in objs.iteritems():
+        o = df_by_obj.loc[t]
+        plt.plot(o.x, o.y)
+    plt.show()
+
+def plots(dataframe):
+    objs = dataframe.groupby(['filename', 'objectNum']).size()
+    df_by_obj = main_df.set_index(['filename', 'objectNum']).sort_index()
+    show(objs,df_by_obj)
 
 
 
-if __name__ == "__main__":
-    import sys
+def start():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
--- Models/Filters.py
+++ Models/Filters.py
@@ -1,44 +1,139 @@
-from pylab import *
-from matplotlib import pyplot as plt
 import pandas as pd
+import six
+from abc import ABCMeta
+
+main_df = pd.read_pickle('../data/paths.pkl.xz')
+decorated_df = main_df
+print(len(main_df))
 
 
-class Filter:
+@six.add_metaclass(ABCMeta)
+class AbstractDataFrame:
+
+    def filter(self):
+        pass
+
+    def get_filter_name(self):
+        pass
+
+
+class ConcreteDataFrame(AbstractDataFrame):
     def __init__(self):
-        print("start filter")
-        self.bg = imread("../paths0.png")
-        self.df = pd.read_pickle('../data/paths.pkl.xz')
+        global decorated_df
+        global main_df
+        decorated_df = main_df
 
-    def located_time(self, s_time, e_time):
-        x = s_time.split(':')
-        y = e_time.split(':')
-        dh = self.df[self.df.time.dt.hour.between(int(x[0]), int(y[0]))]
-        dd = dh[dh.time.dt.minute.between(int(x[1]), int(y[1]))]
-        objs = dd.groupby(['filename', 'objectNum']).size()
-        df_by_obj = dd.set_index(['filename', 'objectNum']).sort_index()
-        return self.plots(objs, df_by_obj)
+    def filter(self):
+        pass
 
-    def located_date(self, date, s_time, e_time,df):
-        x = s_time.split(':')
-        y = e_time.split(':')
-        dates = pd.to_datetime(date)
-        d_d = df[df.time.dt.date == dates.date()]
-        dh = d_d[d_d.time.dt.hour.between(int(x[0]), int(y[0]))]
-        dd = dh[dh.time.dt.minute.between(int(x[1]), int(y[1]))]
-        objs = dd.groupby(['filename', 'objectNum']).size()
-        df_by_obj = dd.set_index(['filename', 'objectNum']).sort_index()
-        return self.plots(objs, df_by_obj)
+    def get_filter_name(self):
+        return 'Concrete Filter'
 
-    def sequence_location(self, x, y, x1, y1):
-        dd = self.df[((self.df.x >= x) & (self.df.x <= x1)) & ((self.df.y >= y) & (self.df.y <= y1))]
-        objs = dd.groupby(['filename', 'objectNum']).size()
-        objs = objs.sample(15)
-        df_by_obj = self.df.set_index(['filename', 'objectNum']).sort_index()
-        return self.plots(objs, df_by_obj)
+    def get_data_frame(self):
+        return decorated_df
 
-    def plots(self, objs, df_by_obj):
-        imshow(self.bg, alpha=0.5)
-        for t, n in objs.iteritems():
-            o = df_by_obj.loc[t]
-            plt.plot(o.x, o.y)
-        return plt
+
+@six.add_metaclass(ABCMeta)
+class AbstractDataFrameDecorator(AbstractDataFrame):
+
+    def __init__(self, decorated_data_frame):
+        self.decorated_data_frame = decorated_data_frame
+
+    def filter(self):
+        return self.decorated_data_frame.filter()
+
+    def get_data_frame(self):
+        return decorated_df
+
+    def get_filter_name(self):
+        return "**" + self.decorated_data_frame.get_filter_name() + "**"
+
+
+class FilterByDateAndTime(AbstractDataFrameDecorator):
+
+    def __init__(self, decorated_data_frame, specific_date, from_time, to_time):
+        AbstractDataFrameDecorator.__init__(self, decorated_data_frame)
+        self.specific_date = specific_date
+        self.from_time = from_time
+        self.to_time = to_time
+
+    def filter(self):
+        global decorated_df
+        self.decorated_data_frame.filter()
+        date_time1 = pd.to_datetime(self.specific_date + ' ' + self.from_time)
+        date_time2 = pd.to_datetime(self.specific_date + ' ' + self.to_time)
+        decorated_df = decorated_df[decorated_df.time.dt.date == date_time1.date()]
+        decorated_df = decorated_df[
+            (decorated_df.time.dt.time >= date_time1.time()) & (decorated_df.time.dt.time <= date_time2.time())]
+
+    def get_filter_name(self):
+        print('Filter by date and time')
+        return 'Filter by date and time'
+
+
+class FilterByTime(AbstractDataFrameDecorator):
+
+    def __init__(self, decorated_data_frame, from_time, to_time):
+        AbstractDataFrameDecorator.__init__(self, decorated_data_frame)
+        self.to_time = to_time
+        self.from_time = from_time
+
+    def filter(self):
+        global decorated_df
+        self.decorated_data_frame.filter()
+        date_time1 = pd.to_datetime(self.from_time)
+        date_time2 = pd.to_datetime(self.to_time)
+        decorated_df = decorated_df[
+            (decorated_df.time.dt.time >= date_time1.time()) & (decorated_df.time.dt.time <= date_time2.time())]
+
+    def get_filter_name(self):
+        print("Filter by time")
+        return "Filter by time"
+
+
+class FilterByLocation(AbstractDataFrameDecorator):
+
+    def __init__(self, decorated_data_frame, coordinate1, coordinate2):
+        AbstractDataFrameDecorator.__init__(self, decorated_data_frame)
+        self.x1, self.y1 = coordinate1
+        self.x2, self.y2 = coordinate2
+
+    def filter(self):
+        global decorated_df
+        self.decorated_data_frame.filter()
+        decorated_df = decorated_df[
+            ((decorated_df.x >= self.x1) & (decorated_df.x <= self.x2))
+            & ((decorated_df.y >= self.y1) & (
+                    decorated_df.y <= self.y2))]
+
+    def get_filter_name(self):
+        print('Filter by location')
+        return 'Filter by location'
+
+
+def arguments_receiver_and_filter(argument_dictionary):
+
+    concrete_data_frame = ConcreteDataFrame()
+    for arg in argument_dictionary.keys():
+        if arg == 'time_filter':
+            start_time = argument_dictionary[arg]['start_time']
+            print(start_time)
+            end_time = argument_dictionary[arg]['end_time']
+            print(end_time)
+            FilterByTime(concrete_data_frame, start_time, end_time)
+
+        if arg == 'date_filter':
+            date = argument_dictionary[arg]['date']
+            start_time = argument_dictionary[arg]['start_time']
+            end_time = argument_dictionary[arg]['end_time']
+            FilterByDateAndTime(concrete_data_frame, date, start_time, end_time)
+
+        if arg == 'location_filter':
+            p1_x = argument_dictionary[arg]['x1']
+            p1_y = argument_dictionary[arg]['y1']
+
+            p2_x = argument_dictionary[arg]['x2']
+            p2_y = argument_dictionary[arg]['y2']
+
+            FilterByLocation(concrete_data_frame, tuple(p1_x, p1_y), tuple(p2_x, p2_y))
+    concrete_data_frame.filter()
